76e504be5b809a2218dba8ce637d47e2f23a1eab
Neb <nebeyo1234@hotmail.com>
2023-04-15 20:41:02 +0800

    .

--- self_test_calculator/src/App.js
+++ self_test_calculator/src/App.js
@@ -1,8 +1,31 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  // Digits: (Input) -> buttons 
+  // Answer: (Output)
+
+  const [digit, changeDigit] = useState('0');
+  const [answer, changeAnswer] = useState();
+
+  //Answer = previous digit (operator + - / *) current digit -> when operator triggers, it begins
+
+  const digitDisplay = (event) =>{
+    event.preventDefault();
+    const digit = event.target.value;
+    changeDigit(digit);
+    
+  }
+
+
   return (
-    <div>
+    
+    <div className="app">
+      <div className='calcWrap'>
+      {digit}
+        <button value="1" onClick={digitDisplay}>1</button>
+      </div>      
     </div>
   );
 }
---
+++ self_test_calculator/src/components/buttonUI.js
@@ -0,0 +1,8 @@
+
+
+
+//if digit or operator do this 
+
+//if equals button do this
+
+//add reset button
